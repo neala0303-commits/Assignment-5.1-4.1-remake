37a0ab4d32837a2ed448bf734e702bb698704e05
neala0303-commits <neala0303@gmail.com>
2026-07-17 11:53:43 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,21 @@
+
+
+let goal = Math.floor(Math.random() * 10) + 1;
+
+function updateText() {
+    let returnValue = Number(prompt("Guess my number"));
+
+    while (returnValue !== goal) {
+        
+        if (returnValue < goal) {
+            returnValue = Number(prompt("Too low! Guess again"));
+        }
+        else if (returnValue > goal) {
+            returnValue = Number(prompt("Too high! Guess again"));
+        }       
+
+     
+    }
+    document.getElementById("heading").innerHTML = "Congratulations! You guessed the number!";
+    
+}
